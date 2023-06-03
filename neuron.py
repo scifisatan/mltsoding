32c95dd393c241c55303a850823d25dccaefee64
scifisatan <brawlershere@gmail.com>
2023-06-03 14:43:37 +0545

    first linear regression model

--- neuron.py
+++ neuron.py
@@ -1,3 +1,5 @@
+import random
+
 dataset = [
     [1,3],
     [2,6],
@@ -10,14 +12,14 @@ dataset = [
 # y = w* x + b 
 
 actualNumber = dataset[0][1]/dataset[0][0]
-w = 2 #initial assumed value
-b = 1
+w = random.random() #initial assumed value
+b = random.random()
 const = rate = 0.00001
 
 # Here the average of distance squared is taken
 def costFunc(w :int, b :int ):
     result = 0
-    # print(f"param = {w}")
+    # print(f"param = {w}, {b}")
     for x in dataset:
         act_y = w * x[0] + b
         exp_y = x[1]
@@ -32,7 +34,7 @@ db = (costFunc(w , (b+const)) - costFunc(w,b))/const
 lowest = costFunc(w, b)
 
 #iteration to get closer to actual value
-for i in range(0,1000):
+for i in range(0,10000):
     w -= rate*dw
     b -= rate*db
     if lowest > costFunc(w,b):#picking the value that gives us the lowest cost
@@ -44,6 +46,7 @@ for i in range(0,1000):
 
 print("-----------------------")
 print(magicalNumber, bias)
-print(f"Our model is {(1 - ( abs(actualNumber - (magicalNumber*1+bias))/actualNumber))*100}% accurate.")
+
+print(costFunc(magicalNumber, bias))
 for x in dataset:
     print(f"{x[0]} | {x[1]} | {x[0]*magicalNumber + bias}")
