cca0deafb4ab9fa302e9a2b583df554000fa24f5
scifisatan <brawlershere@gmail.com>
2023-06-03 14:50:14 +0545

    first linear regression model

--- neuron.py
+++ neuron.py
@@ -2,10 +2,10 @@ import random
 
 dataset = [
     [1,3],
-    [2,6],
-    [3,9],
-    [4,12],
-    [5,15]
+    [2,5],
+    [3,7],
+    [4,9],
+    [5,11]
 ]
 
 # our model
@@ -14,7 +14,7 @@ dataset = [
 actualNumber = dataset[0][1]/dataset[0][0]
 w = random.random() #initial assumed value
 b = random.random()
-const = rate = 0.00001
+const = rate = 0.0001
 
 # Here the average of distance squared is taken
 def costFunc(w :int, b :int ):
@@ -34,7 +34,7 @@ db = (costFunc(w , (b+const)) - costFunc(w,b))/const
 lowest = costFunc(w, b)
 
 #iteration to get closer to actual value
-for i in range(0,10000):
+for i in range(0,1000):
     w -= rate*dw
     b -= rate*db
     if lowest > costFunc(w,b):#picking the value that gives us the lowest cost
